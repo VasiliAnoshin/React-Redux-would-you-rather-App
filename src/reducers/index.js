d1dee128c680e340f248578f970009d2def1013b
Vasili <anoshin45@gmail.com>
2019-03-11 14:05:15 -0700

    update combineREducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import users from './users'
 import { combineReducers } from 'redux'
+import  authedUser  from '../reducers/authedUser'
 
 export default combineReducers({
-    users
+    users,
+    authedUser
 })
