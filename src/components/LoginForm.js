2214c48a83fa0e71135797cd4c6ea0d21448375b
Dev computer <vasilianoshin@alpha-beta.co.il>
2019-03-11 19:49:13 +0200

    fix LoginForm

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,6 +1,7 @@
 import React , {Component} from 'react'
 import { Redirect } from 'react-router-dom'
 import {connect} from 'react-redux'
+import {setAuthedUser} from '../actions/authedUser'
 
 class LoginForm extends Component
 {
@@ -8,6 +9,18 @@ class LoginForm extends Component
     {
         toHome: false,
     }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const {dispatch} = this.props
+        const id = this.selectVal.value
+        dispatch(setAuthedUser(id))
+
+        // this.setState(() => ({
+        //     // toHome: id ? false : true,
+        // }))
+    }
+
     render(){
         const { toHome } = this.props
         if (toHome === true) {
@@ -22,11 +35,10 @@ class LoginForm extends Component
                 </div>
                 <img src ="http://stickersmag.com/wp-content/uploads/2018/03/s-l500.jpg" className = "formAvatar" alt="avatar"></img>
                 <h3 className = 'center'> <span className ='signInTxt'>Sign in </span></h3>
-                <form className='logForm' onSubmit={this.handleSubmit}>
-                    <select className='slct'>
-                    { Object.values(users) !== null  && ( Object.values(users).map((user) => 
-                        // <img src={user.avatarURL} alt="selectLogo"></img>
-                        <option key ={user.id}>
+                <form className='logForm' onSubmit={this.handleSubmit} >
+                    <select className='slct' ref ={(input) => this.selectVal = input}>    
+                    {Object.values(users) !== null  && ( Object.values(users).map((user) => 
+                        <option key ={user.id} value = {user.id} >
                             {user.name}
                         </option>))
                     }
@@ -43,8 +55,7 @@ class LoginForm extends Component
 }
 
 function mapStateToProps(state = {}) {
-    // const { users } = state
-    return { users : state.users }
+    return { users : state.users, dispatch : state }
   }
 
 export default connect(mapStateToProps)(LoginForm)
