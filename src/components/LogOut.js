7f06abacb339c2c608fe7d2d6219c40fe46890da
Dev computer <vasilianoshin@alpha-beta.co.il>
2019-03-12 17:52:21 +0200

    add btn lo logout

--- src/components/LogOut.js
+++ src/components/LogOut.js
@@ -3,14 +3,19 @@ import { connect } from 'react-redux'
 
 class LogOut extends Component 
 {
+    handleLogOut= (e) => {
+        const boom = 'Clac Clac !!!'
+    }
     render(){
     return (
         <nav className='logoutNav'>
           <ul>
             <li>
-                Hello {this.props.id} 
+                Hello {this.props.id}  
                 <img src={this.props.imgUrl} alt='logoUrl'className ='logOutImg'/>
-                
+                {/* <form onSubmit={this.handleSubmit} > */}
+                <button onclick={this.handleLogOut}> LogOut </button>
+                {/* </form> */}
             </li>
           </ul>
         </nav>
