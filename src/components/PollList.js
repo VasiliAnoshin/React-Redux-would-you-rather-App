2fd96fdab80b2e3c42e12cb0c99db15c69cea62c
Vasili <anoshin45@gmail.com>
2019-03-16 04:15:48 -0700

    changes in Poll List component

--- src/components/PollList.js
+++ src/components/PollList.js
@@ -1,14 +1,52 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import Poll from '../components/Poll'
 
 class PollList extends Component {
     
     render(){
         return (
             <div>
-            <span>BLABLABLA</span>
-                </div>
+                <ul>
+                    {
+                        Object.values(this.props.filteredQuestions).map((question) => 
+                        <li key = {question.id}>
+                            <Poll userName = {question.author} imgUrl = {this.props.users[question.author].avatarURL} questionText = {question.optionOne.text}/>
+                        </li>
+                    )}
+                </ul>
+            </div>
         )
     }
 }
 
-export default PollList
+function mapStateToProps({questions, users, authedUser}, sectionChoice ){
+    const userAnswers = users[authedUser].answers
+    console.log(userAnswers)
+    console.log(sectionChoice)
+    const filteredQuestions = (sectionChoice === 2) ?
+     (Object.keys(questions).filter(key => !Object.keys(userAnswers).includes(key))
+    .reduce((obj, key) => {
+        return{
+                ...obj,
+                [key] : questions[key]
+            }
+        },{})) :
+    (Object.keys(questions)
+    .filter(key => Object.keys(userAnswers).includes(key))
+    .reduce((obj, key) => {
+        return{
+                ...obj,
+                [key] : questions[key]
+            }
+        },{}))
+     console.log(filteredQuestions)
+    
+    return {
+        users,
+        filteredQuestions,
+        authedUser
+    }
+}
+
+export default connect(mapStateToProps)(PollList)
