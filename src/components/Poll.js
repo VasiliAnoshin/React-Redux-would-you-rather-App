2c75bb3e775217eb0ec83e7f442cefaa8bf7ac6c
Vasili <anoshin45@gmail.com>
2019-03-16 07:36:42 -0700

    add Poll

--- src/components/Poll.js
+++ src/components/Poll.js
@@ -9,10 +9,22 @@ class Poll extends Component {
         return(
             <div className = "poll">
                 <p className = "pollAuther"> {this.props.userName} asks : </p>
-                <img src={this.props.imgUrl} alt='logoUrl'className ='pollImg '/>
-                <p> Would you rather : </p>
-                <span> {this.props.questionText} </span>
-                <button onClick={this.handleLogOut} > View Poll </button>
+                 <hr/>
+                 <div className ="parent"> 
+                    <div className = "left">
+                        <img src={this.props.imgUrl} alt='logoUrl'className ='pollImg '/>
+                    </div>
+                    <div className = "middle">
+                        <hr width="1" size="100" ></hr>
+                    </div>
+                    <div className = "right">
+                        <p> Would you rather : </p>
+                        <p> {this.props.questionText} </p>
+                        <div>
+                            <button onClick={this.handleLogOut} className = "pollBtn"  align="center"> View Poll </button> 
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
