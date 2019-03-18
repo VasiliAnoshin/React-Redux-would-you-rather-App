90bf0a1e1c252a409564a7570d81a671b6f740e6
Dev computer <vasilianoshin@alpha-beta.co.il>
2019-03-18 13:09:03 +0200

    Add LeaderBoard Poll

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -7,7 +7,35 @@ class LeaderBoard extends Component
         console.log(this.props.users)
         return(
             <div>
-                BlaBLABLA
+                <ul>
+                    {Object.keys(this.props.users).map((key) =>(
+                        <li key ={this.props.users[key].id}>
+                             <div className = "poll">
+                                <p className = "pollAuther"> {this.props.users[key].name} </p>
+                                <hr/>
+                                <div className ="parent">
+                                    <div className = "left">
+                                        <img src={this.props.users[key].avatarURL} alt='logoUrl'className ='pollImg '/>
+                                    </div>
+                                    <div className = "middle">
+                                        <hr width="1" size="100" ></hr>
+                                    </div>
+                                    <p>Answered questions : {Object.keys(this.props.users[key].answers).length}</p>
+                                    <p>Created  questions : {this.props.users[key].questions.length} </p>
+                                    <hr width="1" size="100" ></hr>
+                                    <div className = "right">
+                                        <div className = "smallPoll" >
+                                        <p className = "pollAuther"> Score :  </p>
+                                        <hr/>
+                                            {Object.keys(this.props.users[key].answers).length + this.props.users[key].questions.length}
+                                        </div>
+                                       
+                                    </div>
+                                </div>
+                             </div>
+                        </li>
+                    ))}
+            </ul>
             </div>
         )
     }
@@ -20,7 +48,12 @@ function mapStateToProps({authedUser,users}){
       users : Object.keys(users).sort(function(a,b)
       {
         return (users[b].questions.length + Object.keys(users[b].answers).length)  - (users[a].questions.length + Object.keys(users[a].answers).length)
-      })
+      }).reduce((obj,key) =>{
+            return{
+                ...obj,
+                [key] : users[key]
+            }
+        },{})
     }
   }
 
